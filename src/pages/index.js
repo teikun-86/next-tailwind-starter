212cd92ced355adf73e8b66cc46dd0e1f7ca8780
Aziz Febriyanto <azizfebriyanto12@gmail.com>
2022-12-05 12:25:03 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="w-full min-h-screen grid place-items-center bg-gray-50 p-0 m-0 antialiased">
+            <h1 className="text-black text-2xl font-bold">Next.JS + Tailwind CSS</h1>
+            <p className="text-lg font-semibold text-gray-700">Starter Kit</p>
+        </div>
+  )
+}
