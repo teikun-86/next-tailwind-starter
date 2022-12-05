3a964ea33a8b738553e018e1fc397a34eb612daa
Aziz Febriyanto <azizfebriyanto12@gmail.com>
2022-12-05 12:27:05 +0700

    fix styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 export default function Home() {
     return (
         <div className="w-full min-h-screen grid place-items-center bg-gray-50 p-0 m-0 antialiased">
-            <h1 className="text-black text-2xl font-bold">Next.JS + Tailwind CSS</h1>
-            <p className="text-lg font-semibold text-gray-700">Starter Kit</p>
+            <div>
+                <h1 className="text-black text-2xl font-bold text-center">Next.JS + Tailwind CSS</h1>
+                <p className="text-lg font-semibold text-gray-700 text-center">Starter Kit</p>
+            </div>
         </div>
   )
 }
